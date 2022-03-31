fa4d10629d044e0172e5bf4533fda3c7ee68e134
swapneswarsahu150@gmail.com <swapneswarsahu150@gmail.com>
2022-03-31 21:33:14 +0530

    donate page

---
+++ fstyle.js
@@ -0,0 +1,8 @@
+
+    <script>
+      var rangeLabel = document.getElementById("range-label");
+      var experience = document.getElementById("experience");
+      function change() {
+      rangeLabel.innerText = experience.value + "K"
+      }
+    </script>
